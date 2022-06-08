e2dfab57c50ad29ec22738af2f2b0ec771e8e9d1
Miguel Vital <vital.miguelsantos@gmail.com>
2022-06-08 00:06:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe')
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout (() => {
+        mario.classList.remove('jump');
+    }, 500 );};
+
+    const loop = setInterval(() => {
+        const pipeposition =  pipe.offsetLeft;
+        const marioposition = +window.getComputedStyle(mario).bottom.replace('px','');
+
+        if (pipeposition <= 120 && marioposition < 80 && pipeposition > 0) {
+            pipe.style.animation = 'none';
+            pipe.style.left = `${pipeposition}px`;
+
+            mario.style.animation = 'none';
+            mario.style.bottom = `${marioposition}px`;
+
+            mario.src = './images/game-over.png'
+            mario.style.width = '75px'
+            mario.style.marginLeft = '50px'
+
+            clearInterval(loop);
+        }
+    }, 10)
+
+document.addEventListener('keydown', jump);
+
+//https://c.tenor.com/se0KvlrxCucAAAAj/fnf-sonic.gif
